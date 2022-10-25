3d314976fbb786ccedcff2464b092696c3a34e2b
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-10-25 17:54:14 -0500

    Update section-edit-save.php

--- section-edit-save.php
+++ section-edit-save.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 }
 $iName = $_POST['iName'];
 
-$sql = "update instructor set instructor_name=? where instructor_id=?";
+$sql = "update section set section_number=? where section_id=?";
 //echo $sql;
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("si", $iName, $_POST['iid']);
